68f4b6fc6c0b3187a7096dff8d943142f78ff40d
ApenasGui <guilherme.s.costa1@gmail.com>
2026-03-24 19:46:31 -0300

    Endpoint para pegar os posts do usuário

--- src/main/java/com/guilhermesilva/workshopmongo/Resources/UserResource.java
+++ src/main/java/com/guilhermesilva/workshopmongo/Resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.guilhermesilva.workshopmongo.Resources;
 
+import com.guilhermesilva.workshopmongo.domain.Post;
 import com.guilhermesilva.workshopmongo.domain.User;
 import com.guilhermesilva.workshopmongo.dto.UserDTO;
 import com.guilhermesilva.workshopmongo.service.UserService;
@@ -53,5 +54,12 @@ public class UserResource {
         obj.setId(id);
         obj = service.update(obj);
         return ResponseEntity.noContent().build();
+    }
+
+    @RequestMapping(value = "/{id}/posts", method = RequestMethod.GET)
+    public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
+        User obj = service.findById(id);
+
+        return ResponseEntity.ok().body(obj.getPostsDoUsuario());
     }
 }
